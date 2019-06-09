312a3714fd72315718ce89417a9203b22bfdf41c
adrianxsalazar <adrianxsalazar@gmail.com>
2019-06-09 20:29:09 +0100

    Add files via upload

--- simple_gan._intuition.py
+++ simple_gan._intuition.py
@@ -43,10 +43,10 @@ mnist_dataset.load_data()
 #First Structure: Generator structure
 def generator_structure(input_generator_shape, output__discriminator_shape):
 
-    #The
+    #The generator model is a simple feedforward artificial neural network.
     generator_model=keras.models.Sequential()
 
-    #Normal Dense layer
+    #Normal Dense layer. the input is the what we call the random noise.
     generator_model.add(keras.layers.Dense(256, input_dim=input_shape))
     #We use the activation unit parametric Rectified Linear Unit: Proposed in
     #'Delving Deep into Rectifiers:Surpassing Human-Level Performance on
@@ -59,7 +59,8 @@ def generator_structure(input_generator_shape, output__discriminator_shape):
     generator_model.add(keras.layers.Dense(1024))
     generator_model.add(keras.layers.LeakyReLU(0.2))
 
-    #Output layer
+    #Output layer. The output layer has to have the same number of neurons
+    #as the attributes that define the real data.
     generator_model.add(keras.layers.Dense(output_shape, activation='tanh'))
 
     #Compitle the model we have created. Fix final characteristics of the model.
@@ -71,20 +72,50 @@ def generator_structure(input_generator_shape, output__discriminator_shape):
     #Return the model
     return generator_model
 
-#Put together all the functions we have created to deliver a GANs structure
-
-
 #Create discriminator structure
-def discriminator_model():
-    #
+def discriminator_model(input_dimension):
+    #Feed-fordward model
     discriminator_model=keras.models.Sequential()
-    #
-    discriminator_model.add()
-    #
-    discriminator_model.add()
-    #
 
-    #
+    #Input layer and 2nd layer. The input layer has the same number of neurons
+    #as the number of attributes.
+    discriminator_model.add(keras.layers.Dense(1024, input_dim=input_dimension))
+    discriminator_model.add(keras.layers.LeakyReLU(0.2))
 
-    #
+    #Dropout is a regularization technique to avoid overfitting. The technique
+    #Inhibits randomly some neurons. For more information about dropout:
+    #Dropout: A Simple Way to Prevent Neural Networks from Overfitting
+    discriminator_model.add(keras.layers.Dropout(0.3))
+
+    #Add more layers to the model. Mormal connected layers with LReLU and drop
+    discriminator_model.add(keras.layers.Dense(1024, input_dim=input_dimension))
+    discriminator_model.add(keras.layers.LeakyReLU(0.2))
+    discriminator_model.add(keras.layers.Dropout(0.3))
+    discriminator_model.add(keras.layers.Dense(1024, input_dim=input_dimension))
+    discriminator_model.add(keras.layers.LeakyReLU(0.2))
+
+    #The out layer is binary and differenciates wheter the input is fake
+    discriminator_model.add(keras.layers.Dense(784, activation='tanh'))
+
+    #compile the model
+    discriminator_model.compile(loss='binary_crossentropy',optimizer='adam')
+
     return discriminator_model
+
+#Put together the generator and the discriminator
+def generative_adversarial_model(discriminator, generator):
+    #The command '.trainable=False' excludes a model/layer from training
+    discriminator.trainable=False
+
+    #The input layer initializes a tensor/model
+    keras.layers.Input(shape=)
+
+    #Create
+
+
+
+    return generative_adversarial_model
+
+
+#Visual analysis
+#Put together all the functions we have created to deliver a GANs structure
